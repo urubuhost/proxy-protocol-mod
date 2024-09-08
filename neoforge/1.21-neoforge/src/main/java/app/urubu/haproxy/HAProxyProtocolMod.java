bb8b1af7f634006b87bc810ae5e100868c17ca02
Nick <nick@nickuc.com>
2024-09-08 19:10:47 -0300

    feat: add neoforge support

---
+++ neoforge/1.21-neoforge/src/main/java/app/urubu/haproxy/HAProxyProtocolMod.java
@@ -0,0 +1,9 @@
+package app.urubu.haproxy;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(HAProxyProtocolMod.MODID)
+public class HAProxyProtocolMod
+{
+    public static final String MODID = "haproxy_protocol";
+}
---
+++ neoforge/1.21-neoforge/src/main/java/app/urubu/haproxy/netty/ModHAProxyMessageHandler.java
@@ -0,0 +1,56 @@
+package app.urubu.haproxy.netty;
+
+import io.netty.channel.AbstractChannel;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.haproxy.HAProxyMessage;
+
+import java.lang.reflect.Field;
+import java.net.InetSocketAddress;
+
+public class ModHAProxyMessageHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Field CHANNEL_REMOTE_ADDRESS;
+
+    static {
+        try {
+            CHANNEL_REMOTE_ADDRESS = AbstractChannel.class.getDeclaredField("remoteAddress");
+            CHANNEL_REMOTE_ADDRESS.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (!(msg instanceof HAProxyMessage)) {
+            // Expecting HAProxy message
+            ctx.close();
+            return;
+        }
+
+        HAProxyMessage message = (HAProxyMessage) msg;
+        try {
+            Channel channel = ctx.channel();
+            if (!(channel instanceof AbstractChannel)) {
+                // Expecting AbstractChannel
+                throw new IllegalArgumentException("Unsupported channel type! " + channel.getClass().getCanonicalName());
+            }
+
+            setAddress((AbstractChannel) channel, message);
+            ctx.pipeline().remove(this);
+        } finally {
+            message.release();
+        }
+    }
+
+    private void setAddress(AbstractChannel channel, HAProxyMessage message) {
+        try {
+            CHANNEL_REMOTE_ADDRESS.set(channel,
+                    new InetSocketAddress(message.sourceAddress(), message.sourcePort()));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
